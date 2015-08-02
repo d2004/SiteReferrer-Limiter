1ec952a2a108b90be6e79714b1e3f57e4f5328b4
d2004 <davideramondetti@yahoo.it>
2015-08-02 17:12:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 require_once('SiteReferrer-Limiter/join.php');
+if (CheckReferrer()){
+  echo "welcome<br>";
+} else {
+  echo "denied<br>";
+}
+$loadS1 = new SiteReferrer;
+$getMSG = $loadS1->getMessage();
+print $getMSG;
 ?>
+
+<!-- Powered by, Please do not remove --!>
+<br />
+<p>The Referrers Conditions are Created by <a href="https://github.com/d2004/SiteReferrer-Limited">Davdie Ramondetti</a></p>
+<!-- Behind this box write the page content --!>
+
+<br />
+<p>This is an HTML page</p>

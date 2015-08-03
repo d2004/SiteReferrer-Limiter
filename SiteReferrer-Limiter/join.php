b08745ec4c69b30258fe56a8eae1a4ed228de262
d2004 <davideramondetti@yahoo.it>
2015-08-03 16:55:50 +0200

    Update join.php

--- SiteReferrer-Limiter/join.php
+++ SiteReferrer-Limiter/join.php
@@ -1,1 +1,18 @@
 <?php
+include('config.php');
+class SiteReferrer{
+  public function getMessage(){
+    if (in_array($_SERVER['HTTP_REFERER'],$referrer_true)){
+      return $message_true;
+    } else {
+      return $message_false;
+    }
+  }
+}
+function CheckReferrer(){
+  if (in_array($_SERVER['HTTP_REFERER'],$referrer_true)) {
+    return true;
+  } else {
+    return false;
+  }
+}
